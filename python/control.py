22eb57d2908997744a6bec8f9b99ec5996cb5f34
Aram Baghdasaryan <xquack@gmail.com>
2015-12-20 15:21:44 +0400

    POC is ready

--- python/control.py
+++ python/control.py
@@ -1,17 +1,34 @@
 import httplib2
 import json
+import RPi.GPIO as GPIO
+
+# Terminal color
+(c_null, c_red, c_green, c_header) = ('\033[0m', '\033[91m', '\033[92m', '\033[95m')
+pin_list = [21, 20, 16, 26, 19, 13, 6, 5]
 
 def init():
-    print('initiated...')
+    print(c_header + 'initiated...' + c_null)
 
     config = get_config()
     (status, device) = get_device(config['device_id'])
 
+    config_GPIO()
+
     if status:
-        print('{0} found and it\'s {1}'.format(device['name'], 'active' if int(device['status']) == 1 else 'inactive'))
+        print(c_header + '{0} found and it\'s {1}'.format(device['name'], device['status']) + c_null)
+
+        for port in device['ports']:
+            setup_GPIO(port, device['ports'][port]['status'])
     else:
-        print('Error occured')
-    # print(status, devices)
+        print(c_red + 'Error occured' + c_null)
+
+def setup_GPIO(port_id, status):
+    port_id = pin_list[int(port_id) - 1]
+
+    if str(status) == 'active':
+        GPIO.setup(port_id, GPIO.OUT, initial=GPIO.LOW)
+    else:
+        GPIO.setup(port_id, GPIO.OUT, initial=GPIO.HIGH)
 
 def get_device(device_id):
     status = False;
@@ -42,6 +59,10 @@ def get_config():
     return {
         'device_id': 1
     }
+
+def config_GPIO():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
 
 if __name__ == '__main__':
     init()
